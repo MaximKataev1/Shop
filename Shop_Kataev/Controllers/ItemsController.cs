44d8c3226d5926f227a177eef3401d2060df77d8
MaximKataev1 <maximkataev503@gmail.com>
2025-10-22 11:56:34 +0500

    Разработанный контроллер предметов. Катаев

---
+++ Shop_Kataev/Controllers/ItemsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop_Kataev.Data.Interfaces;
+
+namespace Shop_Kataev.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private IItems IAllItems;
+        private ICategories IAllCategorys;
+
+        public ItemsController(IItems IAllItems, ICategories IAllCategorys) {        
+            this.IAllItems = IAllItems;
+            this.IAllCategorys = IAllCategorys;
+        }
+
+        public ViewResult List() 
+        {
+            ViewBag.Title = "Страница с предметами";
+
+            var cars = IAllItems.AllItems;
+            return View(cars);
+        }
+ 
+    }
+}
--- Shop_Kataev/Program.cs
+++ Shop_Kataev/Program.cs
@@ -1,4 +1,9 @@
+using Shop_Kataev.Data.Interfaces;
+using Shop_Kataev.Data.Mocks;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<ICategories, MockCaregorys>();
+builder.Services.AddTransient<IItems, MockItems>();
 builder.Services.AddMvc(option => option.EnableEndpointRouting = false);
 var app = builder.Build();
 
