0d00822547493a84fcff28b7f66e899b29aa2cfa
MaximKataev1 <maximkataev503@gmail.com>
2025-10-26 22:50:20 +0500

    Разработанное перенаправление на страницу с предметами. Катаев

---
+++ Shop_Kataev/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop_Kataev.Controllers
+{
+    public class HomeController : Controller
+    {
+        public RedirectResult Index() 
+        {
+            return Redirect("/Items/List");
+        }
+    }
+}
