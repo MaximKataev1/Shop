26f9b042b00c8ec677a9ad3816ddb3255bdcbe83
MaximKataev1 <maximkataev503@gmail.com>
2025-10-26 20:36:14 +0500

    Метод добавления предмета. Катаев

--- Shop_Kataev/Controllers/ItemsController.cs
+++ Shop_Kataev/Controllers/ItemsController.cs
@@ -7,13 +7,15 @@ namespace Shop_Kataev.Controllers
 {
     public class ItemsController : Controller
     {
+        private readonly Microsoft.AspNetCore.Hosting.IHostingEnvironment hostingEnvironment;
         private IItems IAllItems;
         private ICategories IAllCategorys;
         VMItems VMItems = new VMItems();
 
-        public ItemsController(IItems IAllItems, ICategories IAllCategorys) {
+        public ItemsController(IItems IAllItems, ICategories IAllCategorys, Microsoft.AspNetCore.Hosting.IHostingEnvironment hostingEnvironment) {
             this.IAllItems = IAllItems;
             this.IAllCategorys = IAllCategorys;
+            this.hostingEnvironment = hostingEnvironment;
         }
 
         public ViewResult List(int id = 0)
@@ -37,5 +39,25 @@ namespace Shop_Kataev.Controllers
             return View(Categorys);
         }
 
+        [HttpPost]
+        public RedirectResult Add(string name, string description, IFormFile files, float price, int idCategory) 
+        {
+            if (files != null) 
+            {
+                var uploads = Path.Combine(hostingEnvironment.WebRootPath, "img");
+                var filePath = Path.Combine(uploads, files.FileName);
+                files.CopyTo(new FileStream(filePath, FileMode.Create));
+            }
+            Items newItems = new Items();
+            newItems.Name = name;
+            newItems.Description = description;
+            newItems.Img = "/img/" + Path.GetFileName(files.FileName);
+            newItems.Price = Convert.ToInt32(price);
+            newItems.Category = new Categories() { Id = idCategory };
+
+            int id = IAllItems.Add(newItems);
+            return Redirect("/Items/Update?id=" + id);
+
+        }
     } 
 }
